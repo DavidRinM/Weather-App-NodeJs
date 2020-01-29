f27dae402b13a2204a8fb42f7685a84554136079
David Rincon <david.rincon.morales@outlook.com>
2020-01-28 20:54:17 -0600

    Handling Errors

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
 const request = require("request");
 
+const weatherurl =
+  "https://api.darksky.net/forecast/2cb01e9bccbb3592ce5c8b6b98897ffb/37.8267,-122.4233?units=si&lang=es";
+const geocodingurl =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZGF2aWRyaW5tIiwiYSI6ImNrNXllemx6NzJmdmMza24xd20yYmY0aTYifQ.a1jV-KyIifqTZzRhxMujmQ&limit=1";
 
-const weatherurl = "https://api.darksky.net/forecast/2cb01e9bccbb3592ce5c8b6b98897ffb/37.8267,-122.4233?units=si&lang=es";
-const geocodingurl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZGF2aWRyaW5tIiwiYSI6ImNrNXllemx6NzJmdmMza24xd20yYmY0aTYifQ.a1jV-KyIifqTZzRhxMujmQ&limit=1";
-
-request({ url:weatherurl, json:true }, (error, response) => {
+request({ url: weatherurl, json: true }, (error, response) => {
+  if (error) {
+    console.log("Unable to connect to Weather Service");
+  } else if (response.body.error) {
+    console.log("Unable to find Location");
+  } else {
     const temperature = response.body.currently.temperature;
     const precipitation = response.body.currently.precipProbability;
     const dailyWeather = response.body.daily.data[0].summary;
-
-    console.log(dailyWeather + " It´s currently " + temperature + " degrees outside. There´s " + precipitation + "% of rain");
+    console.log(
+      dailyWeather +
+        " It´s currently " +
+        temperature +
+        " degrees outside. There´s " +
+        precipitation +
+        "% of rain"
+    );
+  }
 });
 
-request({ url:geocodingurl, json:true }, (error, response) => {
+request({ url: geocodingurl, json: true }, (error, response) => {
+  if (error) {
+    console.log("Unable to connect to location services");
+  } else if (response.body.features.length === 0) {
+    console.log("Coordenates not found. Try another search");
+  } else {
     const longitud = response.body.features[0].center[0];
     const latitude = response.body.features[0].center[1];
     console.log("Latitud: " + latitude + " Longitud: " + longitud);
+  }
 });
